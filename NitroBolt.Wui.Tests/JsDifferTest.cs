9309b511573ad13487cf41565dce0050f9c1c80b
sbessmertnykh <bessmertnykh@payqr.ru>
2015-08-20 20:42:40 +0300

    tests

---
+++ NitroBolt.Wui.Tests/JsDifferTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace NitroBolt.Wui.Tests
+{
+    [TestClass]
+    public class JsDifferTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var results = HtmlJavaScriptDiffer.JsSync(new HElementProvider(), h.Div(), h.Div("txt"), null).ToArray();
+            var s = JsonConvert.SerializeObject(results);
+            Assert.AreEqual("[{'path':null,'cmd':'set','value':{'a':[],'t':{'value':'txt'},'h':null}}]".Replace('\'', '"'), s);
+        }
+
+        public readonly static HBuilder h = null;
+    }
+}
