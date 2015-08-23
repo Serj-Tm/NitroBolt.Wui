1287fca0b58279d1a0e59899b6f5fc117929def5
Serj-Tm <serj-tm@ya.ru>
2015-08-23 12:59:36 +0300

    nuspec projectUrl tags

--- NitroBolt.Wui.Tests/JsDifferTest.cs
+++ NitroBolt.Wui.Tests/JsDifferTest.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
+using NitroBolt.Functional;
 
 namespace NitroBolt.Wui.Tests
 {
@@ -9,11 +10,28 @@ namespace NitroBolt.Wui.Tests
     public class JsDifferTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void NewText()
         {
-            var results = HtmlJavaScriptDiffer.JsSync(new HElementProvider(), h.Div(), h.Div("txt"), null).ToArray();
+            Assert.AreEqual("[{'path':[],'cmd':'set','value':{'a':[],'t':{'value':'txt'},'h':null}}]", Diff(h.Div(), h.Div("txt")));
+        }
+        [TestMethod]
+        public void RemoveElement()
+        {
+            Assert.AreEqual("[{'path':[{'kind':'element','index':1}],'cmd':'remove'},{'path':[{'kind':'element','index':0}],'cmd':'after','value':{'name':'div'}},{'path':[{'kind':'element','index':2}],'cmd':'remove'}]", 
+                Diff(h.Div(h.Div(),h.Span(), h.Div()), h.Div(h.Div(), h.Div())));
+        }
+        [TestMethod]
+        public void RemoveSingleElement()
+        {
+            Assert.AreEqual("[{'path':[{'kind':'element','index':0}],'cmd':'remove'}]",
+                Diff(h.Div(h.Span()), h.Div()));
+        }
+
+        private static string Diff(HElement h1, HElement h2)
+        {
+            var results = HtmlJavaScriptDiffer.JsSync(new HElementProvider(), h1, h2, Array<object>.Empty).ToArray();
             var s = JsonConvert.SerializeObject(results);
-            Assert.AreEqual("[{'path':null,'cmd':'set','value':{'a':[],'t':{'value':'txt'},'h':null}}]".Replace('\'', '"'), s);
+            return s.Replace('"', '\'');
         }
 
         public readonly static HBuilder h = null;
