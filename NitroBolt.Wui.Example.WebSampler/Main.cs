96dcaa82d0861b6c1c4c4973309cf12e39c36481
- <->
2016-02-26 23:17:12 +0300

    radio button

--- NitroBolt.Wui.Example.WebSampler/Main.cs
+++ NitroBolt.Wui.Example.WebSampler/Main.cs
@@ -24,8 +24,14 @@ namespace NitroBolt.WebSampler
                         state = state.With(text: json.JPath("value")?.ToString());
                         System.Threading.Thread.Sleep(new Random().Next(0, 500));
                         break;
+                    case "link":
+                        state = state.With(text: "self:" + new Random().Next(1000));
+                        break;
                     case "error":
                         throw new Exception("ошибка");
+                    case "x":
+                        state = state.With(text: json.JPath("value")?.ToString());
+                        break;
                     default:
                         break;
                 }
@@ -59,10 +65,17 @@ namespace NitroBolt.WebSampler
                 h.Input(h.type("button"), h.onclick(";"), new hdata { { "command", "error" } }, h.value("throw error")),
                 h.Div(state.Text),
                 h.Div(h.A(h.href("multi.html"), "multi sync frames")),
-                h.Div(h.A(h.href("auth-view.html?id=12"), "auth-view"))
+                h.Div(h.A(h.href("auth-view.html?id=12"), "auth-view")),
+                h.Div(h.A(h.href("#"), "self-link", h.onclick("e.stopPropagation();"), h.data("command", "link"))),
               //h.Div(DateTime.UtcNow),
               //h.Input(h.type("button"), h.onclick(";"), h.value("update")),
               //h.Div(1, h.Attribute("js-init", "$(this).css('color', 'red')"))
+                h.Div
+                (
+                   h.Input(h.type("radio"), h.Attribute("name", "x"), h.value("v1"), h.onclick(";"), new hdata { { "command", "x" } }), h.Span("V1"),
+                   h.Input(h.type("radio"), h.Attribute("name", "x"), h.value("v2"), h.onclick(";"), h.@checked(), new hdata { { "command", "x" } }), h.Span("V2"),
+                   h.Input(h.type("radio"), h.Attribute("name", "x"), h.value("v3"), h.onclick(";"), new hdata { { "command", "x" } }), h.Span("V3")
+                )
               )
             );
             return page;
