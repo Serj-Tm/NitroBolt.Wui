0fe0a0560de4d78d0e2206f27c459bd129783d0b
- <->
2016-06-28 20:04:38 +0300

    WebSampler - raw html sample added

--- NitroBolt.Wui.Example.WebSampler/Controllers/MainController.cs
+++ NitroBolt.Wui.Example.WebSampler/Controllers/MainController.cs
@@ -20,7 +20,21 @@ namespace NitroBolt.WebSampler
             return HWebApiSynchronizeHandler.Process<object>(this.Request, HView);
         }
 
-        public static NitroBolt.Wui.HtmlResult<HElement> HView(object _state, JsonData[] jsons, HttpRequestMessage request)
+        [HttpGet, HttpPost]
+        [Route("main-raw")]
+        public HttpResponseMessage RouteRaw()
+        {
+            return HWebApiSynchronizeHandler.Process<object>(this.Request, HViewRaw);
+        }
+
+        static HtmlResult HViewRaw(object _state, JsonData[] jsons, HttpRequestMessage request)
+        {
+            var result = HView(_state, jsons, request);
+            result.FirstHtmlTransformer = html => html;
+            return result;
+        }
+
+        public static HtmlResult HView(object _state, JsonData[] jsons, HttpRequestMessage request)
         {
             var state = _state.As<MainState>() ?? new MainState();
 
@@ -51,7 +65,7 @@ namespace NitroBolt.WebSampler
 
 
             var page = Page(state);
-            return new NitroBolt.Wui.HtmlResult<HElement>
+            return new HtmlResult
             {
                 Html = page,
                 State = state,
@@ -76,9 +90,9 @@ namespace NitroBolt.WebSampler
                 h.Input(h.type("text"), h.Attribute("onkeyup", ";"), new hdata { { "command", "text" } }),
                 h.Input(h.type("button"), h.onclick(";"), new hdata { { "command", "error" } }, h.value("throw error")),
                 h.Div(state.Text),
+                h.Div(h.A(h.href(""), "main"), h.Span("("), h.A(h.href("main-raw"), "raw"), h.Span(")"), h.A(h.href("#"), "self-link", h.onclick("e.stopPropagation();"), h.data("command", "link"))),
                 h.Div(h.A(h.href("multi.html"), "multi sync frames")),
                 h.Div(h.A(h.href("auth?id=12"), "auth-view")),
-                h.Div(h.A(h.href("#"), "self-link", h.onclick("e.stopPropagation();"), h.data("command", "link"))),
               //h.Div(DateTime.UtcNow),
               //h.Input(h.type("button"), h.onclick(";"), h.value("update")),
               //h.Div(1, h.Attribute("js-init", "$(this).css('color', 'red')"))
