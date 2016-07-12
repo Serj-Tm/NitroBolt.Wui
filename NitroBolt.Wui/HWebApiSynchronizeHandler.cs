fcc592bc61012531c9d616cbd4ce034cd83c4cfd
Serj-Tm <Serj-tm@ya.ru>
2016-07-12 19:33:45 +0300

    HWebApiSync: error handling changed

--- NitroBolt.Wui/HWebApiSynchronizeHandler.cs
+++ NitroBolt.Wui/HWebApiSynchronizeHandler.cs
@@ -54,33 +54,29 @@ namespace NitroBolt.Wui
 
                 var json_commands = (json.JPath("commands").As<JArray>()?.Select(j => new JsonData(j)).ToArray()).OrEmpty();
 
+                HtmlResult<HElement> result = null;
+                var watch = System.Diagnostics.Stopwatch.StartNew();
                 try
                 {
-
-                    var watch = System.Diagnostics.Stopwatch.StartNew();
-                    var result = page(prev?.Item2?.State.As<TState>() ?? new TState(), json_commands, request);
-                    watch.Stop();
-                    if (result.As<HtmlResult>()?.RawResponse != null)
-                        return result.As<HtmlResult>()?.RawResponse;
-
-                    var isPartial = result.Html.Name.LocalName != "html";
-                    var toBody = isPartial ? html => html : (Func<HElement, HElement>)(html => html?.Element("body"));
-
-                    var js_updates = HtmlJavaScriptDiffer.JsSync(new HElementProvider(), toBody(prev?.Item2?.Page), toBody(result.Html)).ToArray();
-                    var jupdate = new Dictionary<string, object>() { { "cycle", prev.Item1 }, { "prev_cycle", cycle }, { "processed_commands", json_commands.Length }, { "updates", js_updates } };
-
-                    PushUpdate(frame, prev.Item1, result.Html, result.State, watch.Elapsed);
-
-                    return ApplyProcessor(new HttpResponseMessage() { Content = new StringContent(JsonConvert.SerializeObject(jupdate), Encoding.UTF8, "application/javascript") }, result);
+                    result = page(prev?.Item2?.State.As<TState>() ?? new TState(), json_commands, request);
                 }
-                catch (Exception exc)//HACK ловятся все ошибки для того, чтобы не зациклилась страница. Добавить обработку ошибок на сторону js.
+                catch (Exception exc) //HACK ловятся все ошибки для того, чтобы не зациклилась страница. Добавить обработку ошибок на сторону js.
                 {
-                    
-                    var jupdate = new Dictionary<string, object>() { { "cycle", prev.Item1 }, { "prev_cycle", cycle }, { "processed_commands", json_commands.Length }, { "updates", new object[] { } } };
-
-                    return ApplyProcessor(new HttpResponseMessage() { Content = new StringContent(JsonConvert.SerializeObject(jupdate), Encoding.UTF8, "application/javascript") }, null);
-
+                    result = new HtmlResult<HElement> { State = prev.Item2.State, Html = prev.Item2.Page };
                 }
+                watch.Stop();
+                if (result.As<HtmlResult>()?.RawResponse != null)
+                    return result.As<HtmlResult>()?.RawResponse;
+
+                var isPartial = result.Html.Name.LocalName != "html";
+                var toBody = isPartial ? html => html : (Func<HElement, HElement>)(html => html?.Element("body"));
+
+                var js_updates = HtmlJavaScriptDiffer.JsSync(new HElementProvider(), toBody(prev?.Item2?.Page), toBody(result.Html)).ToArray();
+                var jupdate = new Dictionary<string, object>() { { "cycle", prev.Item1 }, { "prev_cycle", cycle }, { "processed_commands", json_commands.Length }, { "updates", js_updates } };
+
+                PushUpdate(frame, prev.Item1, result.Html, result.State, watch.Elapsed);
+
+                return ApplyProcessor(new HttpResponseMessage() { Content = new StringContent(JsonConvert.SerializeObject(jupdate), Encoding.UTF8, "application/javascript") }, result);
             }
         }
         static HElement FirstHtmlTransformer(HElement element)
