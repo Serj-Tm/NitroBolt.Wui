50a070a8d4ed94034e8268364ff07b937697d4a2
Serj-Tm <Serj-tm@ya.ru>
2016-10-24 00:29:19 +0300

    HWebApiSynchronizeHandler: make HttpRequestMessage adapter

--- NitroBolt.Wui/HWebApiSynchronizeHandler.cs
+++ NitroBolt.Wui/HWebApiSynchronizeHandler.cs
@@ -23,11 +23,17 @@ namespace NitroBolt.Wui
 
         public static HttpResponseMessage Process<TState>(HttpRequestMessage request, Func<TState, JsonData[], HttpRequestMessage, HtmlResult<HElement>> page) where TState : class, new()
         {
-            if (request.Method == HttpMethod.Get)
+            return Process(request, HttpRequestMessageAdapter.Instance, page);
+        }
+
+        public static TResponse Process<TRequest, TResponse, TState>(TRequest request, IRequestAdapter<TRequest, TResponse> requestAdapter, Func<TState, JsonData[], TRequest, HtmlResult<HElement>> page) where TState : class, new()
+        {
+            if (requestAdapter.IsGetMethod(request))
             {
                 var result = page(new TState(), Array<JsonData>.Empty, request);
-                if (result.As<HtmlResult>()?.RawResponse != null)
-                    return result.As<HtmlResult>()?.RawResponse;
+                var rawResponse = requestAdapter.RawResponse(result);
+                if (rawResponse != null)
+                    return rawResponse;
 
                 var html = result.Html;
                 var head = html.Element("head") ?? new HElement("head");
@@ -40,13 +46,13 @@ namespace NitroBolt.Wui
                 var firstHtmlTransformer = result.As<HtmlResult>()?.FirstHtmlTransformer ?? FirstHtmlTransformer;
                 html = firstHtmlTransformer(new HElement("html", startHead, html.Nodes.Where(node => node.As<HElement>()?.Name.LocalName != "head")));
                 var toHtmlText = result.As<HtmlResult>()?.ToHtmlText ?? ToHtmlText;
-                return ApplyProcessor(new HttpResponseMessage() { Content = new StringContent(toHtmlText(html), Encoding.UTF8, "text/html") }, result);
+                return requestAdapter.ToResponse(toHtmlText(html), "text/html", result);
             }
             else
             {
-                var json = Parse(request.Content.ReadAsStringAsync().Result);
+                var json = Parse(requestAdapter.Content(request));
 
-                var route = request.GetRouteData()?.Route?.RouteTemplate ?? "<null>";
+                var route = requestAdapter.Frame(request) ?? "<null>";
 
                 var frame = route + ":" + json.JPath("frame")?.ToString();
                 var cycle = ConvertHlp.ToInt(json.JPath("cycle")).OrDefault(0);
@@ -65,8 +71,9 @@ namespace NitroBolt.Wui
                     result = new HtmlResult<HElement> { State = prev.Item2.State, Html = prev.Item2.Page };
                 }
                 watch.Stop();
-                if (result.As<HtmlResult>()?.RawResponse != null)
-                    return result.As<HtmlResult>()?.RawResponse;
+                var rawResponse = requestAdapter.RawResponse(result);
+                if (rawResponse != null)
+                    return rawResponse;
 
                 var isPartial = result.Html.Name.LocalName != "html";
                 var toBody = isPartial ? html => html : (Func<HElement, HElement>)(html => html?.Element("body"));
@@ -76,7 +83,7 @@ namespace NitroBolt.Wui
 
                 PushUpdate(frame, prev.Item1, result.Html, result.State, watch.Elapsed);
 
-                return ApplyProcessor(new HttpResponseMessage() { Content = new StringContent(JsonConvert.SerializeObject(jupdate), Encoding.UTF8, "application/javascript") }, result);
+                return requestAdapter.ToResponse(JsonConvert.SerializeObject(jupdate), "application/javascript", result);
             }
         }
         static HElement FirstHtmlTransformer(HElement element)
@@ -87,16 +94,7 @@ namespace NitroBolt.Wui
         {
             return element?.ToHtmlText();
         }
-        static HttpResponseMessage ApplyProcessor(HttpResponseMessage response, HtmlResult<HElement> result)
-        {
-            var apiResult = result as HtmlResult;
-            if (apiResult != null && apiResult.ResponseProcessor != null)
-            {
-                apiResult.ResponseProcessor(response);
-            }
 
-            return response;
-        }
 
         static JsonData Parse(string t)
         {
@@ -176,7 +174,7 @@ namespace NitroBolt.Wui
 
     }
 
-    public class HtmlResult:HtmlResult<HElement>
+    public class HtmlResult : HtmlResult<HElement>
     {
         public Action<HttpResponseMessage> ResponseProcessor = null;
         public Func<HElement, HElement> FirstHtmlTransformer = null;
@@ -192,5 +190,45 @@ namespace NitroBolt.Wui
         /// Имеет силу только при первой загрузке страницы
         /// </summary>
         public TimeSpan? RefreshPeriod;
+    }
+    public interface IRequestAdapter<TRequest, TResponse>
+    {
+        bool IsGetMethod(TRequest request);
+        string Content(TRequest request);
+        string Frame(TRequest request);
+
+        TResponse RawResponse(HtmlResult<HElement> result);
+
+        TResponse ToResponse(string content, string contentType, HtmlResult<HElement> result);
+    }
+
+    public class HttpRequestMessageAdapter : IRequestAdapter<HttpRequestMessage, HttpResponseMessage>
+    {
+        public static readonly HttpRequestMessageAdapter Instance = new HttpRequestMessageAdapter();
+
+        public bool IsGetMethod(HttpRequestMessage request) => request.Method == HttpMethod.Get;
+
+        public string Content(HttpRequestMessage request) => request.Content.ReadAsStringAsync().Result;
+
+
+        public string Frame(HttpRequestMessage request) => request.GetRouteData()?.Route?.RouteTemplate;
+
+        public HttpResponseMessage RawResponse(HtmlResult<HElement> result) => result.As<HtmlResult>()?.RawResponse;
+
+        public HttpResponseMessage ToResponse(string content, string contentType, HtmlResult<HElement> result)
+        {
+            return ApplyProcessor(new HttpResponseMessage() { Content = new StringContent(content, Encoding.UTF8, contentType) }, result);
+        }
+
+        static HttpResponseMessage ApplyProcessor(HttpResponseMessage response, HtmlResult<HElement> result)
+        {
+            var apiResult = result as HtmlResult;
+            if (apiResult != null && apiResult.ResponseProcessor != null)
+            {
+                apiResult.ResponseProcessor(response);
+            }
+
+            return response;
+        }
     }
 }
