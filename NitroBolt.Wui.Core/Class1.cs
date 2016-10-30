464876f58349145a6e05ffab776801a0adf1c685
Serj-Tm <Serj-tm@ya.ru>
2016-10-30 18:44:48 +0300

    Asp.Net core - first version

--- NitroBolt.Wui.Core/Class1.cs
+++ NitroBolt.Wui.Core/Class1.cs
@@ -1,14 +1,38 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace NitroBolt.Wui
+namespace NitroBolt.Wui.Core
 {
-    public class Class1
+    public static class HWebApiSynchronizeHandler
     {
-        public Class1()
+        public static IActionResult Process<TState>(HttpRequest request, Func<TState, JsonData[], HttpRequest, HtmlResult<HElement>> page) where TState : class, new()
         {
+            return NitroBolt.Wui.HWebApiSynchronizeHandler.Process(request, HttpRequestAdapter.Instance, page);
         }
+    }
+
+    public class HttpRequestAdapter : IRequestAdapter<HttpRequest, IActionResult>
+    {
+        public static readonly HttpRequestAdapter Instance = new HttpRequestAdapter();
+
+        public bool IsGetMethod(HttpRequest request) => request.Method == "GET";
+
+        public string Content(HttpRequest request) => new StreamReader(request.Body).ReadToEnd();
+
+
+        public string Frame(HttpRequest request) => "main";
+
+        public IActionResult RawResponse(HtmlResult<HElement> result) => null;
+
+        public IActionResult ToResponse(string content, string contentType, HtmlResult<HElement> result)
+        {
+            return new ContentResult() {Content = content, ContentType = contentType };
+        }
+
     }
 }
